a6fe777743b089399dfcaf61629d7b742312ac90
kongkunn <congdeptraiii102@gmail.com>
2023-04-17 16:39:37 +0700

    first commit

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -7,17 +7,14 @@ const Head = () => {
         <div className='container d_flex'>
           <div className='left row'>
             <i className='fa fa-phone'></i>
-            <label> +88012 3456 7894</label>
+            <label> 08799.12855</label>
             <i className='fa fa-envelope'></i>
-            <label> support@ui-lib.com</label>
+            <label> cogntran2000@gmail.com</label>
           </div>
           <div className='right row RText'>
             <label>Theme FAQ"s</label>
             <label>Need Help?</label>
-            <span>🏳️‍⚧️</span>
-            <label>EN</label>
-            <span>🏳️‍⚧️</span>
-            <label>USD</label>
+           
           </div>
         </div>
       </section>
